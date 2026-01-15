12554fc5e164d5e83d493ebf3634f33c4b7fccae
Milagros del Carmen Acuña <milagrosdelc49@gmail.com>
2026-01-14 23:19:12 -0300

    realice el ejercicio 8 en la rama feature/logica

--- js/tp1Ejercicios.js
+++ js/tp1Ejercicios.js
@@ -58,3 +58,18 @@ else{
     mayor = num3;
 }
 console.log(`El ${mayor} es el número más grande`);
+
+/*8.- Escribe un programa que pida un número y diga si es divisible por 2
+Ejemplo: 
+input: 10
+input: 15
+Output: El 10 es divisible por 2.
+Output: El 15 no es divisible por 2.
+Dificultad:  🟢🟡 */
+let numero = parseInt (prompt("Ingrese un numero"))
+if (numero % 2 === 0) {
+    console.log(`El ${numero} es divisible por 2.`)
+}
+else {
+    console.log(`El ${numero} no es divisible por 2.`)
+}
