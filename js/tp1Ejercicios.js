79f7a263cb269858e21beca3dd25e5ad031455a2
Milagros del Carmen Acuña <milagrosdelc49@gmail.com>
2026-01-15 09:54:45 -0300

    realice el ejercicio 10 en la rama feature/logica

--- js/tp1Ejercicios.js
+++ js/tp1Ejercicios.js
@@ -95,3 +95,22 @@ for (let i = 0; i < frase.length; i++) {
         console.log(letra);
     }
 }
+
+/*10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+Ejemplo: 
+input: 20
+Output: El 20 es divisible por 2.
+Dificultad:  🟢🟡*/
+let numeros = Number(prompt("Introduce un número:"));
+
+if (numeros % 2 === 0) {
+    console.log(`El ${numeros} es divisible por 2.`);
+} else if (numeros % 3 === 0) {
+    console.log(`El ${numeros} es divisible por 3.`);
+} else if (numeros % 5 === 0) {
+    console.log(`El ${numeros} es divisible por 5.`);
+} else if (numeros % 7 === 0) {
+    console.log(`El ${numeros} es divisible por 7.`);
+} else {
+    console.log(`El ${numeros} no es divisible por 2, 3, 5 ni 7.`);
+}
