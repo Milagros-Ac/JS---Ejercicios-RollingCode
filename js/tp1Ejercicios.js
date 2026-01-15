a46acaaa41b36d1853c484958014c57776938b11
Milagros del Carmen Acuña <milagrosdelc49@gmail.com>
2026-01-14 23:16:57 -0300

    realice el ejercicio 4 en la rama feature/logica

--- js/tp1Ejercicios.js
+++ js/tp1Ejercicios.js
@@ -9,3 +9,11 @@ document.writeln("Hello world")
 /*3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
 Dificultad:  🟢*/
 document.writeln(3+5)
+
+/*4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+Ejemplo: 
+input: Coder 
+Output: Hola Coder
+Dificultad:  🟢*/
+let Coder = prompt("Ingrese su nombre")
+document.writeln("Hola "+ Coder)
